da863496f3576d7794784215419104c1e4a35710
taokyone <antonsson.andreas@hotmail.com>
2023-03-27 11:18:34 +0200

    setting up projects folder structure

---
+++ app/src/main/java/com/taokyone/aa_finalproject_android/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.taokyone.aa_finalproject_android.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taokyone.aa_finalproject_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
