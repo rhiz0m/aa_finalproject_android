bfe57d219778ee09eb3d279deaa6d4d4a0d15d67
taokyone <antonsson.andreas@hotmail.com>
2023-03-30 20:31:42 +0200

    Implement Retrofit, create Modelclass and RetrofitAPI interface

---
+++ app/src/main/java/com/taokyone/aa_finalproject_android/model/CharacterInfo.kt
@@ -0,0 +1,11 @@
+package com.taokyone.aa_finalproject_android.model
+
+// Model class for Character
+
+data class CharacterInfo(
+    //val img: String,
+    val name: String,
+    val aliases: String,
+    val deck: String
+) {
+}
--- app/src/main/java/com/taokyone/aa_finalproject_android/view/MainActivity.kt
+++ app/src/main/java/com/taokyone/aa_finalproject_android/view/MainActivity.kt
@@ -2,11 +2,66 @@ package com.taokyone.aa_finalproject_android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.taokyone.aa_finalproject_android.R
+import com.taokyone.aa_finalproject_android.databinding.ActivityMainBinding
+import com.taokyone.aa_finalproject_android.model.CharacterInfo
+import com.taokyone.aa_finalproject_android.model.apiData.RetroFitAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val baseURL = "https://comicvine.gamespot.com/"
+    lateinit var viewBinding: ActivityMainBinding
+    var characterInfoList = ArrayList<CharacterInfo>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = viewBinding.root
+        setContentView(view)
+        showPosts()
+    }
+
+
+    fun showPosts() {
+        val retroFit = Retrofit.Builder().
+        baseUrl(baseURL).
+        addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val retrofitAPI : RetroFitAPI = retroFit.create(RetroFitAPI::class.java)
+
+        val call : Call<List<CharacterInfo>> = retrofitAPI.getCharacterInfo()
+
+        // Callback function
+        call.enqueue(object : Callback<List<CharacterInfo>> {
+            override fun onResponse(call: Call<List<CharacterInfo>>, response: Response<List<CharacterInfo>>) {
+
+                if(!response.isSuccessful) {
+                    //viewBinding.imgCharacterId
+                    viewBinding.tvNameId.text = "Error - name couldn't be found"
+                    viewBinding.tvAliasesId.text = "Error - aliases couldn't be found"
+                    viewBinding.tvDeckId.text = "Error - breif cescription couln't be found"
+                }
+
+                characterInfoList = response.body() as ArrayList<CharacterInfo>
+                viewBinding.tvNameId.text = characterInfoList[0].name
+                viewBinding.tvNameId.text = characterInfoList[0].aliases
+                viewBinding.tvNameId.text = characterInfoList[0].deck
+
+            }
+
+            override fun onFailure(call: Call<List<CharacterInfo>>, t: Throwable) {
+                Toast.makeText(applicationContext,
+                    t.localizedMessage,
+                    Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
